63a9e2b44355e73749a636bacead25d93211cc46
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-06 08:45:00 +0300

    solved

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,19 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        if (sideLength <= 0) {
+            return;
+        }
+        int mid = sideLength / 2 + 1;
+        for (int i = 1; i <= sideLength; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if (i == mid || j == mid) {
+                    System.out.print("8");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
